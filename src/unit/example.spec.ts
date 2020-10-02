3561ddb7228a8c46068ad817b0aece0604f9a591
Alex Gellert <alexgellert1@gmail.com>
2020-10-02 16:57:22 -0400

    first jest test

---
+++ src/unit/example.spec.ts
@@ -0,0 +1,9 @@
+import { shallowMount } from '@vue/test-utils'
+import Home from './../components/Home.vue'
+
+describe('Home.vue', () => {
+  it('renders props.msg when passed', () => {
+    const wrapper = shallowMount(Home);
+    expect(wrapper.isVueInstance()).toBeTruthy();
+  })
+})
